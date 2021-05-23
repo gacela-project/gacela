f37a864639b181cec75fe1ba52bb2778571ff922
JesusValera <jesus1902@outlook.com>
2021-05-23 16:43:23 +0200

    Allow subfolders on config dir

--- tests/Integration/Framework/UsingConfig/LocalConfig/Config.php
+++ tests/Integration/Framework/UsingConfig/LocalConfig/Config.php
@@ -14,6 +14,7 @@ final class Config extends AbstractConfig
             'config' => (int)$this->get('config'),
             'config_local' => (int)$this->get('config_local'),
             'override' => (int)$this->get('override'),
+            'subfolder' => (int)$this->get('subfolder'),
         ];
     }
 }

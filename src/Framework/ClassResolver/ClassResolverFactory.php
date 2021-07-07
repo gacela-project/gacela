382111d1e1db14a2b1dcfbd9af0ae2317586c217
Chemaclass <chemaclass@outlook.es>
2021-07-07 18:18:23 +0200

    Simplify AbstractClassResolver

--- src/Framework/ClassResolver/ClassResolverFactory.php
+++ src/Framework/ClassResolver/ClassResolverFactory.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Gacela\Framework\ClassResolver;
 
-use Gacela\Framework\AbstractFactory;
 use Gacela\Framework\ClassResolver\ClassNameFinder\ClassNameFinder;
 use Gacela\Framework\ClassResolver\ClassNameFinder\ClassNameFinderInterface;
 use Gacela\Framework\ClassResolver\ClassNameFinder\Rule\FinderRuleWithModulePrefix;
 use Gacela\Framework\ClassResolver\ClassNameFinder\Rule\FinderRuleWithoutModulePrefix;
 
-final class ClassResolverFactory extends AbstractFactory
+final class ClassResolverFactory
 {
     public function createClassNameFinder(): ClassNameFinderInterface
     {

3396de7b01cc2802dc97ef03d067475b0d5e5f26
JesusValera <jesus1902@outlook.com>
2021-08-13 00:56:29 +0200

    Add feature tests

---
+++ tests/Feature/CodeGenerator/MakeFileCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GacelaTest\Feature\CodeGenerator;
+
+use GacelaTest\Feature\Util\DirectoryUtil;
+use PHPUnit\Framework\TestCase;
+
+final class MakeFileCommandTest extends TestCase
+{
+    private const ENTRY_POINT = __DIR__ . '/../../../';
+
+    /**
+     * @dataProvider createFilesProvider
+     */
+    public function test_make_file(string $action, string $fileName, string $shortName): void
+    {
+        DirectoryUtil::removeDir(self::ENTRY_POINT . 'src/TestModule');
+
+        $command = sprintf('%sgacela make:file %s Gacela/TestModule %s', self::ENTRY_POINT, $shortName, $action);
+        exec($command, $output);
+
+        self::assertSame("> Path 'src/TestModule/{$fileName}.php' created successfully", $output[0]);
+        self::assertFileExists(self::ENTRY_POINT . "src/TestModule/{$fileName}.php");
+
+        DirectoryUtil::removeDir(self::ENTRY_POINT . 'src/TestModule');
+        self::assertDirectoryDoesNotExist(self::ENTRY_POINT . 'src/TestModule');
+    }
+
+    public function createFilesProvider(): iterable
+    {
+        yield 'facade' => ['facade', 'TestModuleFacade', ''];
+        yield 'factory' => ['factory', 'TestModuleFactory', ''];
+        yield 'config' => ['config', 'TestModuleConfig', ''];
+        yield 'dependency provider' => ['dependency-provider', 'TestModuleDependencyProvider', ''];
+
+        // Sort name flag
+        yield 'facade -s' => ['facade', 'Facade', '-s'];
+        yield 'factory -s' => ['factory', 'Factory', '-s'];
+        yield 'config -s' => ['config', 'Config', '-s'];
+        yield 'dependency provider -s' => ['dependency-provider', 'DependencyProvider', '-s'];
+    }
+}

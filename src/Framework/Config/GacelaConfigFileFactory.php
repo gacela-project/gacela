64ffe040b2454f59931c45d69153e3236f6d63e2
Chemaclass <chemaclass@outlook.es>
2021-12-31 17:29:30 +0100

    Refactoring GacelaConfigFileFactory->createGacelaFileConfig()

--- src/Framework/Config/GacelaConfigFileFactory.php
+++ src/Framework/Config/GacelaConfigFileFactory.php
@@ -7,7 +7,6 @@ namespace Gacela\Framework\Config;
 use Gacela\Framework\AbstractConfigGacela;
 use Gacela\Framework\Config\GacelaFileConfig\GacelaConfigFile;
 use RuntimeException;
-use function is_array;
 use function is_callable;
 
 final class GacelaConfigFileFactory implements GacelaConfigFileFactoryInterface
@@ -36,46 +35,42 @@ final class GacelaConfigFileFactory implements GacelaConfigFileFactoryInterface
         $gacelaPhpPath = $this->applicationRootDir . '/' . $this->gacelaPhpConfigFilename;
 
         if (!is_file($gacelaPhpPath)) {
-            if (isset($this->globalServices['config'])) {
-                /** @var array{
-                 *     config: array<array>|array{type:string,path:string,path_local:string},
-                 *     mapping-interfaces: array<string,string|callable>,
-                 * } $configFromGlobalServices
-                 */
-                $configFromGlobalServices = $this->globalServices;
-
-                return GacelaConfigFile::fromArray($configFromGlobalServices);
-            }
-
-            return GacelaConfigFile::withDefaults();
+            return $this->createDefaultGacelaPhpConfig();
         }
 
         /** @var array|callable|mixed $configGacela */
         $configGacela = include $gacelaPhpPath;
 
-        if (is_callable($configGacela)) {
-            /** @var AbstractConfigGacela $configGacelaClass */
-            $configGacelaClass = $configGacela($this->globalServices);
-            if (!is_subclass_of($configGacelaClass, AbstractConfigGacela::class)) {
-                throw new RuntimeException('Your anonymous class must extends AbstractConfigGacela');
-            }
-
-            /** @psalm-suppress ArgumentTypeCoercion */
-            return GacelaConfigFile::fromArray([
-                'config' => $configGacelaClass->config(),
-                'mapping-interfaces' => $configGacelaClass->mappingInterfaces(),
-            ]);
+        if (!is_callable($configGacela)) {
+            throw new RuntimeException('Create a function that returns an anonymous class that extends AbstractConfigGacela');
         }
 
-        if (is_array($configGacela)) {
-            trigger_error('Use a function that returns an anonymous class that extends AbstractConfigGacela. Check the documentation for more info. Array is deprecated.', E_USER_DEPRECATED);
+        /** @var AbstractConfigGacela $configGacelaClass */
+        $configGacelaClass = $configGacela($this->globalServices);
+        if (!is_subclass_of($configGacelaClass, AbstractConfigGacela::class)) {
+            throw new RuntimeException('Your anonymous class must extends AbstractConfigGacela');
+        }
+
+        /** @psalm-suppress ArgumentTypeCoercion */
+        return GacelaConfigFile::fromArray([
+            'config' => $configGacelaClass->config(),
+            'mapping-interfaces' => $configGacelaClass->mappingInterfaces(),
+        ]);
+    }
+
+    private function createDefaultGacelaPhpConfig(): GacelaConfigFile
+    {
+        if (isset($this->globalServices['config'])) {
             /** @var array{
              *     config: array<array>|array{type:string,path:string,path_local:string},
              *     mapping-interfaces: array<string,string|callable>,
-             * } $configGacela */
-            return GacelaConfigFile::fromArray($configGacela);
+             * } $configFromGlobalServices
+             */
+            $configFromGlobalServices = $this->globalServices;
+
+            return GacelaConfigFile::fromArray($configFromGlobalServices);
         }
 
-        throw new RuntimeException('Create a function that returns an anonymous class that extends AbstractConfigGacela');
+        return GacelaConfigFile::withDefaults();
     }
 }

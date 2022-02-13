e63348bfd42848ac407b824040ea3d4281599e0b
JesusValera <jesus1902@outlook.com>
2022-02-13 15:13:43 +0100

    Add documentation to AbstractConfigGacela class

--- src/Framework/AbstractConfigGacela.php
+++ src/Framework/AbstractConfigGacela.php
@@ -9,8 +9,18 @@ use Gacela\Framework\Config\ConfigReaderInterface;
 abstract class AbstractConfigGacela
 {
     /**
+     * e.g:
+     * <code>
+     * return [
+     *   'path' => '.env*',
+     *   'path_local' => '.env',
+     * ];
+     * </code>
+     *
+     * <b>path</b>: Define the path where Gacela will read all the config files. Default: <i>config/*.php</i><br>
+     * <b>path_local</b>: Define the path where Gacela will read the local config file. Default: <i>config/local.php</i>
+     *
      * @return array<array>|array{
-     *     type?:string,
      *     path?:string,
      *     path_local?:string
      * }
@@ -21,6 +31,13 @@ abstract class AbstractConfigGacela
     }
 
     /**
+     * e.g:
+     * <code>
+     * return ['php' => new \Gacela\Framework\Config\ConfigReader\PhpConfigReader()];
+     * </code>
+     *
+     * Define the reader class which will read and parse the config files. Default: <i>PhpConfigReader</i>
+     *
      * @return array<string,ConfigReaderInterface>
      */
     public function configReaders(): array
@@ -29,6 +46,15 @@ abstract class AbstractConfigGacela
     }
 
     /**
+     * e.g:
+     * <code>
+     * return [
+     *     Abstractclass::class => new SpecificClass($dependencies),
+     * ];
+     * </code>
+     *
+     * Define the mapping between interfaces and concretions, so, Gacela will auto-resolve them automatically.
+     *
      * @param array<string,mixed> $globalServices
      *
      * @return array<class-string,class-string|callable>
@@ -39,6 +65,8 @@ abstract class AbstractConfigGacela
     }
 
     /**
+     * Define path/s where Gacela should check when an interface should be auto-resolve.
+     *
      * @return list<string>
      */
     public function customServicePaths(): array

bde2fc6095d220a53ed6c7dc200817083ae1277c
Chemaclass <chemaclass@outlook.es>
2022-05-14 14:37:26 +0200

    Add missing deprecated methods for deprecated SetupGacela.
    We need to allow the old method names (with a deprecated notice) before removing them

--- src/Framework/Setup/SetupGacela.php
+++ src/Framework/Setup/SetupGacela.php
@@ -5,10 +5,36 @@ declare(strict_types=1);
 namespace Gacela\Framework\Setup;
 
 use Gacela\Framework\Bootstrap\SetupGacela as BootstrapSetupGacela;
+use Gacela\Framework\Config\GacelaConfigBuilder\ConfigBuilder;
+use Gacela\Framework\Config\GacelaConfigBuilder\MappingInterfacesBuilder;
+use Gacela\Framework\Config\GacelaConfigBuilder\SuffixTypesBuilder;
 
 /**
  * @deprecated in favor of \Gacela\Framework\Bootstrap\SetupGacela
  */
 final class SetupGacela extends BootstrapSetupGacela
 {
+    /**
+     * @deprecated in favor of setConfigBuilder()
+     */
+    public function setConfig(ConfigBuilder $builder): BootstrapSetupGacela
+    {
+        return $this->setConfigBuilder($builder);
+    }
+
+    /**
+     * @deprecated in favor of setMappingInterfacesBuilder()
+     */
+    public function setMappingInterfaces(MappingInterfacesBuilder $builder): BootstrapSetupGacela
+    {
+        return $this->setMappingInterfacesBuilder($builder);
+    }
+
+    /**
+     * @deprecated in favor of setSuffixTypesBuilder()
+     */
+    public function setSuffixTypes(SuffixTypesBuilder $builder): BootstrapSetupGacela
+    {
+        return $this->setSuffixTypesBuilder($builder);
+    }
 }

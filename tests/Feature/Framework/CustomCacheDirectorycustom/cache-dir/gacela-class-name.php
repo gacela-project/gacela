6829e57a7e1c4500f7720fc07412151f2c199d49
Chemaclass <chemaclass@outlook.es>
2022-10-16 11:59:35 +0200

    Delete unnecesary gacela-class-name.php files

--- tests/Feature/Framework/CustomCacheDirectorycustom/cache-dir/gacela-class-name.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-return [
-  '\\GacelaTest\\Feature\\Framework\\CustomCacheDirectory\\Module\\Persistence\\FakeRepository' => '\\GacelaTest\\Feature\\Framework\\CustomCacheDirectory\\Module\\Persistence\\FakeRepository',
-];
--- tests/Integration/Framework/DocBlockResolverAware.gacela/cache/gacela-class-name.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-return [
-  '\\GacelaTest\\Integration\\Framework\\DocBlockResolverAware\\Persistence\\FakeRepository' => '\\GacelaTest\\Integration\\Framework\\DocBlockResolverAware\\Persistence\\FakeRepository',
-];

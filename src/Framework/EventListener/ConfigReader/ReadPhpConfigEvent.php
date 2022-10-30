d43d1679ffcd7b944a2452ef62e9b439e2428071
JesusValera <jesus1902@outlook.com>
2022-10-30 12:08:08 +0100

    Add ReadPhpConfigEvent & GacelaConfigReaderListener

---
+++ src/Framework/EventListener/ConfigReader/ReadPhpConfigEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gacela\Framework\EventListener\ConfigReader;
+
+use Gacela\Framework\EventListener\AbstractGacelaEvent;
+
+final class ReadPhpConfigEvent extends AbstractGacelaEvent
+{
+}

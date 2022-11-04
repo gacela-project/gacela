772f7ca247a9b8049e7acb8c17ce2371054f38c5
Chemaclass <chemaclass@outlook.es>
2022-11-04 22:48:52 +0100

    Add ClassNameFinder events

--- src/Framework/ClassResolver/ClassNameFinder/ClassNameFinder.php
+++ src/Framework/ClassResolver/ClassNameFinder/ClassNameFinder.php
@@ -7,9 +7,16 @@ namespace Gacela\Framework\ClassResolver\ClassNameFinder;
 use Gacela\Framework\ClassResolver\Cache\CacheInterface;
 use Gacela\Framework\ClassResolver\ClassInfo;
 use Gacela\Framework\ClassResolver\ClassNameFinder\Rule\FinderRuleInterface;
+use Gacela\Framework\Event\ClassResolver\ClassNameFinder\ClassNameCachedFound;
+use Gacela\Framework\Event\ClassResolver\ClassNameFinder\ClassNameInvalidCandidateFound;
+use Gacela\Framework\Event\ClassResolver\ClassNameFinder\ClassNameNotFound;
+use Gacela\Framework\Event\ClassResolver\ClassNameFinder\ClassNameValidCandidateFound;
+use Gacela\Framework\Event\Dispatcher\EventDispatchingCapabilities;
 
 final class ClassNameFinder implements ClassNameFinderInterface
 {
+    use EventDispatchingCapabilities;
+
     private ClassValidatorInterface $classValidator;
 
     /** @var list<FinderRuleInterface> */
@@ -46,7 +53,10 @@ final class ClassNameFinder implements ClassNameFinderInterface
         $cacheKey = $classInfo->getCacheKey();
 
         if ($this->cache->has($cacheKey)) {
-            return $this->cache->get($cacheKey);
+            $cached = $this->cache->get($cacheKey);
+            $this->dispatchEvent(new ClassNameCachedFound($cached));
+
+            return $cached;
         }
         $projectNamespaces = $this->projectNamespaces;
         $projectNamespaces[] = $classInfo->getModuleNamespace();
@@ -55,14 +65,20 @@ final class ClassNameFinder implements ClassNameFinderInterface
             foreach ($this->finderRules as $finderRule) {
                 foreach ($resolvableTypes as $resolvableType) {
                     $className = $finderRule->buildClassCandidate($projectNamespace, $resolvableType, $classInfo);
+
                     if ($this->classValidator->isClassNameValid($className)) {
                         $this->cache->put($cacheKey, $className);
+                        $this->dispatchEvent(new ClassNameValidCandidateFound($className));
 
                         return $className;
                     }
+
+                    $this->dispatchEvent(new ClassNameInvalidCandidateFound($className));
                 }
             }
         }
+
+        $this->dispatchEvent(new ClassNameNotFound($classInfo, $resolvableTypes));
 
         return null;
     }
---
+++ src/Framework/Event/ClassResolver/ClassNameFinder/ClassNameInvalidCandidateFound.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gacela\Framework\Event\ClassResolver\ClassNameFinder;
+
+use Gacela\Framework\Event\GacelaEventInterface;
+
+final class ClassNameInvalidCandidateFound implements GacelaEventInterface
+{
+    private string $className;
+
+    public function __construct(string $className)
+    {
+        $this->className = $className;
+    }
+
+    public function toString(): string
+    {
+        return sprintf('%s - %s', self::class, $this->className);
+    }
+}

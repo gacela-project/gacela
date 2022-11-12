1da6801bffcc840bf374692b445e4de404d47846
Chemaclass <chemaclass@outlook.es>
2022-11-12 15:14:04 +0100

    Refactor and simplify Container tests

--- tests/Unit/Framework/Container/ContainerTest.php
+++ tests/Unit/Framework/Container/ContainerTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace GacelaTest\Unit\Framework\Container;
 
 use Gacela\Framework\Container\Container;
+use Gacela\Framework\Container\Exception\ContainerException;
 use Gacela\Framework\Container\Exception\ContainerKeyNotFoundException;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 final class ContainerTest extends TestCase
 {
@@ -64,17 +66,68 @@ final class ContainerTest extends TestCase
 
     public function test_resolve_service_as_callable_class(): void
     {
-        $this->container->set('service_name', new class() {
-            public function __invoke(): string
-            {
-                return 'value';
+        $this->container->set(
+            'service_name',
+            new class() {
+                public function __invoke(): string
+                {
+                    return 'value';
+                }
             }
-        });
+        );
 
         $resolvedService = $this->container->get('service_name');
         self::assertSame('value', $resolvedService);
 
         $cachedResolvedService = $this->container->get('service_name');
         self::assertSame('value', $cachedResolvedService);
+    }
+
+    public function test_resolve_non_factory_service_with_random(): void
+    {
+        $this->container->set(
+            'service_name',
+            new class() {
+                public function __invoke(): string
+                {
+                    return 'value_' . random_int(0, PHP_INT_MAX);
+                }
+            }
+        );
+
+        self::assertSame(
+            $this->container->get('service_name'),
+            $this->container->get('service_name')
+        );
+    }
+
+    public function test_resolve_factory_service_with_random(): void
+    {
+        $this->container->set(
+            'service_name',
+            $this->container->factory(
+                new class() {
+                    public function __invoke(): string
+                    {
+                        return 'value_' . random_int(0, PHP_INT_MAX);
+                    }
+                }
+            )
+        );
+
+        self::assertNotSame(
+            $this->container->get('service_name'),
+            $this->container->get('service_name')
+        );
+    }
+
+    public function test_resolve_factory_service_not_invokable(): void
+    {
+        $this->expectExceptionObject(ContainerException::serviceNotInvokable());
+
+        $this->container->set(
+            'service_name',
+            $this->container->factory(new stdClass())
+        );
     }
 }

85d8e3193a353e91e6bdfa0bd42e0115dfb3b768
Chemaclass <chemaclass@outlook.es>
2023-03-19 00:32:38 +0100

    Allow using factory() or getFactory() from Facade

---
+++ tests/Feature/Framework/StaticFacade/ModuleA/Facade.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GacelaTest\Feature\Framework\StaticFacade\ModuleA;
+
+use Gacela\Framework\AbstractFacade;
+
+/**
+ * @method static Factory factory()
+ * @method Factory getFactory()
+ */
+final class Facade extends AbstractFacade
+{
+    public static function createStringFromStaticFactory(): string
+    {
+        return self::factory()->createString();
+    }
+
+    public function createStringFromNonStaticFactory(): string
+    {
+        return $this->getFactory()->createString();
+    }
+}
--- tests/Feature/Framework/StaticFacade/ModuleB/Facade.php
+++ tests/Feature/Framework/StaticFacade/ModuleB/Facade.php
@@ -2,23 +2,23 @@
 
 declare(strict_types=1);
 
-namespace GacelaTest\Feature\Framework\StaticFacade\Module;
+namespace GacelaTest\Feature\Framework\StaticFacade\ModuleB;
 
 use Gacela\Framework\AbstractFacade;
 
 /**
- * @method static Factory factory()
- * @method Factory getFactory()
+ * @method Factory factory()
+ * @method static Factory getFactory()
  */
 final class Facade extends AbstractFacade
 {
     public static function createStringFromStaticFactory(): string
     {
-        return self::factory()->createString();
+        return self::getFactory()->createString();
     }
 
     public function createStringFromNonStaticFactory(): string
     {
-        return $this->getFactory()->createString();
+        return $this->factory()->createString();
     }
 }

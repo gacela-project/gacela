0ab3eb2022e585bb5d5a404aac8e1fc2672bd54a
JesusValera <jesus1902@outlook.com>
2023-05-30 15:03:45 +0200

    Create FilterListModulesCommandTest

---
+++ tests/Feature/Console/ListModules/FilterListModulesCommandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GacelaTest\Feature\Console\ListModules;
+
+use Gacela\Console\Infrastructure\ConsoleBootstrap;
+use Gacela\Framework\Gacela;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+final class FilterListModulesCommandTest extends TestCase
+{
+    public function test_list_modules(): void
+    {
+        Gacela::bootstrap(__DIR__);
+
+        $input = new StringInput('list:modules -f ' . __DIR__ . '/TestModule1');
+        $output = new BufferedOutput();
+
+        $bootstrap = new ConsoleBootstrap();
+        $bootstrap->setAutoExit(false);
+        $bootstrap->run($input, $output);
+
+        $out = $output->fetch();
+
+        self::assertMatchesRegularExpression('#TestModule1.*ListModules\\\TestModule1#', $out);
+        self::assertStringNotContainsString('TestModule2', $out);
+        self::assertStringNotContainsString('TestModule3', $out);
+        self::assertStringNotContainsString('vendor', $out);
+        self::assertStringNotContainsString('ToBeIgnored', $out);
+    }
+}

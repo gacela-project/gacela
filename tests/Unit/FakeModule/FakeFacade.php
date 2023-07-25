b36f53da8ab355e4577b01f61e9b1f564bbee2fc
Chemaclass <chemaclass@outlook.es>
2023-07-25 09:30:17 +0200

    Add Unir/FakeModule for unit tests

---
+++ tests/Unit/FakeModule/FakeFacade.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GacelaTest\Unit\FakeModule;
+
+use Gacela\Framework\AbstractFacade;
+
+/**
+ * @method FakeFactory getFactory()
+ */
+final class FakeFacade extends AbstractFacade
+{
+}
---
+++ tests/Unit/Framework/ClassResolver/DocBlockService/DocBlockServiceNotFoundExceptionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace GacelaTest\Unit\Framework\ClassResolver\DocBlockService;
+
+
+final class DocBlockServiceNotFoundExceptionTest
+{
+
+}

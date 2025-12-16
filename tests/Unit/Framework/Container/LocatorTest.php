30d2e899ecbfc1543cfca86b7f6d4c9093329acc
Chemaclass <chemaclass@outlook.es>
2025-12-16 08:36:21 +0100

    feat: add Gacela::getRequired()

--- tests/Unit/Framework/Container/LocatorTest.php
+++ tests/Unit/Framework/Container/LocatorTest.php
@@ -6,6 +6,7 @@ namespace GacelaTest\Unit\Framework\Container;
 
 use Gacela\Framework\Container\Container;
 use Gacela\Framework\Container\Locator;
+use Gacela\Framework\Exception\ServiceNotFoundException;
 use GacelaTest\Fixtures\StringValue;
 use PHPUnit\Framework\TestCase;
 
@@ -54,5 +55,50 @@ final class LocatorTest extends TestCase
         $singleton = Locator::getSingleton(StringValue::class, $container);
 
         self::assertEquals(new StringValue('str'), $singleton);
+    }
+
+    public function test_get_required_throws_when_not_found(): void
+    {
+        $this->expectException(ServiceNotFoundException::class);
+        $this->expectExceptionMessage('Service "GacelaTest\Unit\Framework\Container\NonExisting" not found in the container.');
+
+        Locator::getInstance()->getRequired(NonExisting::class);
+    }
+
+    public function test_get_required_singleton_throws_when_not_found(): void
+    {
+        $this->expectException(ServiceNotFoundException::class);
+        $this->expectExceptionMessage('Service "GacelaTest\Unit\Framework\Container\NonExisting" not found in the container.');
+
+        Locator::getRequiredSingleton(NonExisting::class);
+    }
+
+    public function test_get_required_returns_existing_service(): void
+    {
+        Locator::addSingleton(StringValue::class, new StringValue('required'));
+
+        $singleton = Locator::getInstance()->getRequired(StringValue::class);
+
+        self::assertEquals(new StringValue('required'), $singleton);
+    }
+
+    public function test_get_required_singleton_returns_existing_service(): void
+    {
+        Locator::addSingleton(StringValue::class, new StringValue('required'));
+
+        $singleton = Locator::getRequiredSingleton(StringValue::class);
+
+        self::assertEquals(new StringValue('required'), $singleton);
+    }
+
+    public function test_get_required_singleton_from_container(): void
+    {
+        $container = new Container(bindings: [
+            StringValue::class => new StringValue('from-container'),
+        ]);
+
+        $singleton = Locator::getRequiredSingleton(StringValue::class, $container);
+
+        self::assertEquals(new StringValue('from-container'), $singleton);
     }
 }
